8b5e830ec85517fdb372526450c273ee9202b6c0
bagalmahesh <91608124+bagalmahesh@users.noreply.github.com>
2022-06-01 20:36:37 +0530

    Add files via upload

---
+++ ValidateEmail.php
@@ -0,0 +1,27 @@
+<html>
+<body>  <?php
+$emailErr ="";
+$email=" ";
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+  function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  E-mail: <input type="text" name="email">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+   <input type="submit" name="submit" value="Submit">  
+ </form>
+ </body>
+</html>
