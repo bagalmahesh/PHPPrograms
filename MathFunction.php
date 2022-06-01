80964f007e32e387f5e4a6c8470dda21371f0800
bagalmahesh <91608124+bagalmahesh@users.noreply.github.com>
2022-06-01 18:43:26 +0530

    Add files via upload

---
+++ MathFunction.php
@@ -0,0 +1,9 @@
+<?php
+echo(pi())."<br>";
+echo(min(0, 150, 30, 20, -8, -200))."<br>";  
+echo(max(0, 150, 30, 20, -8, -200))."<br>"; 
+echo(abs(-6.7))."<br>";
+echo(sqrt(64))."<br>"; 
+echo(round(0.60))."<br>";
+echo(rand())."<br>";
+?>
